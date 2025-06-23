f5d1968b7e98af998a49e9fdef17d73794928db2
MayankAsutkar <mayankasutkar17102006@gmail.com>
2025-06-24 01:39:37 +0530

    dqueue

---
+++ stack and queue/deque.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+class dqueue{
+    int start;
+    int end;
+    public:
+    int arr[100000];
+    dqueue()
+    {
+        start = 0;
+        end = -1;
+    }
+    void popf()
+    {
+        end--;
+    }
+    void popb()
+    {
+        start++;
+    }
+    int front()
+    {
+        return arr[end];
+    }
+    int back()
+    {
+        return arr[start];
+    }
+    void push(int x)
+    {
+        end++;
+        arr[end] = x;
+    }
+    bool empty()
+    {
+        if(start>end)
+        return true;
+        else
+        return false;
+    }
+};
+int main()
+{
+    dqueue dqueue;
+    dqueue.push(1);
+    dqueue.push(3);
+    dqueue.push(4);
+    dqueue.popf();
+    dqueue.push(5);
+    dqueue.push(6);
+    dqueue.popb();
+    
+    cout<<dqueue.front();
+    cout<<dqueue.back();
+}
